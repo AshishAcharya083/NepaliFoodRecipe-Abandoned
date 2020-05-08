f960dbcb2f8477759655da4237164ffa29ca3260
Ashish <imashish083@gmail.com>
2020-05-08 19:35:51 +0545

    used Align in stack insted of positioned

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -10,7 +10,7 @@ class _ListCardState extends State<ListCard> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 0,vertical: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 20),
       child: Column(
         
         mainAxisAlignment: MainAxisAlignment.center,
@@ -19,56 +19,71 @@ class _ListCardState extends State<ListCard> {
           Expanded(
               child: ListWheelScrollView.useDelegate(
             diameterRatio: 1.2,
-            perspective: 0.0000000001,
+            perspective: 0.0001,
+           
             itemExtent: 150,
             childDelegate: ListWheelChildBuilderDelegate(
                 builder: (BuildContext context, int index) {
               if (index < 0 || index > 10) {
                 return null;
               }
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Stack(
-                  
-                  children: [
-                  // Container(
-                  //   height: 275.0,
-                  //   width: 200.0,
-                  //   color: Colors.transparent,
-                  // ),
-                  Positioned(
-                      top: 20.0,
-                      child: Container(
-                          height: 100.0,
-                          width: MediaQuery.of(context).size.width *0.7,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            color: Colors.lightGreen,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[],
-                          ),
-                        )),
-                  Positioned(
-                      left: 200.0,
-                      bottom: 50,
-                      child: Container(
-                        // height: 110.0,
-                        // width: 110.0,
-                        constraints: BoxConstraints(
-                          maxHeight:100.0,
-                          maxWidth: 100.0 
-                        ),
+              return Stack(
+                alignment: Alignment.center,
+                
+                children: [
+                // Container(
+                //   height: 275.0,
+                //   width: 200.0,
+                //   color: Colors.transparent,
+                // ),
+                Align(
+                  alignment: Alignment.center,
+                    // top: 20.0,
+                    child: Container(
+                        height: 120.0,
+                        width: MediaQuery.of(context).size.width ,
                         decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                image: AssetImage('images/burger.jpg'),
-                                fit: BoxFit.cover)),
-                      ))
-                ]),
-              );
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.lightGreen,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[],
+                        ),
+                      )),
+                Align(
+                  
+                  alignment: Alignment(0.8, -1),
+                    // left: 200.0,
+                    // bottom: 50,
+                    child: Container(
+                      
+                      // height: 110.0,
+                      // width: 110.0,
+                      constraints: BoxConstraints(
+                        maxHeight:110.0,
+                        maxWidth: 110.0 
+                      ),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+      BoxShadow(
+        color: Colors.green.withOpacity(0.9),
+        spreadRadius: 3,
+        blurRadius: 10,
+         // changes position of shadow
+      ),
+    ],
+                       border: Border.all(
+                         width: 3,
+                         color: Colors.green
+                       ),
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: AssetImage('images/burger.jpg'),
+                              fit: BoxFit.cover)),
+                    ))
+              ]);
             }),
           )),
         ],
