44dac924eb7a2782bb4be2ebff462a96ce95a910
Ashish <imashish083@gmail.com>
2020-05-12 18:53:04 +0545

    added sliver list

--- lib/cooking_screen.dart
+++ lib/cooking_screen.dart
@@ -4,41 +4,59 @@ class CookingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
-              child: CustomScrollView(
-                slivers: <Widget>[
-                  SliverAppBar(
-                   
-                    
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                    expandedHeight: 200.0,
-                    floating: false,
-                    pinned: true,
-                    flexibleSpace: FlexibleSpaceBar(
-                     
-                     collapseMode: CollapseMode.parallax,
-                      background: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(bottomRight: Radius.circular(30),bottomLeft: Radius.circular(30)),
-                          image: DecorationImage(image: AssetImage('images/burger.jpg'),
-                          fit: BoxFit.cover
-                          )
-                        ),
-                       
-                      )
-                    ),
-                  ),
-                  SliverFillRemaining(
-                    child: Center(
-                      child: Text('This is the center of the widget'),
-                    ),
-                  )
-                ],
+        child: CustomScrollView(
+          
+          physics: BouncingScrollPhysics(),
+          slivers: <Widget>[
+            SliverAppBar(
+              
+              backgroundColor: Colors.blue,
+              elevation: 2,
+              expandedHeight: 200.0,
+              floating: true,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+               
+                
+                  collapseMode: CollapseMode.parallax,
+                  background: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(30),
+                            bottomLeft: Radius.circular(30)),
+                        image: DecorationImage(
+                            image: AssetImage('images/burger.jpg'),
+                            fit: BoxFit.cover)),
+                  )),
+            ),
+            SliverList(delegate: SliverChildListDelegate([
+ Text(
+                        'This is text',
+                        style: TextStyle(fontSize: 50),
+                      ),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50)),
+                      Text('This is text', style: TextStyle(fontSize: 50))
+            ]))
+            
+          ],
         ),
-        
       ),
-      
     );
   }
 }
